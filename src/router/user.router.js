c184565c45bb021a0dadf26ac7ca3cc9aeca5f6b
arizalinside <arizalinc@gmail.com>
2021-07-09 18:04:23 +0700

    created jwt for authenticated user

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { insertUser, getUserByEmail } = require("../model/user/User.model");
 const { hashPassword, comparePassword } = require("../helpers/bcrypt.helper");
+const { createAccessJWT, createRefreshJWT } = require("../helpers/jwt.helper");
 
 router.all("/", (req, res, next) => {
   // res.json({ message: "return form user router" })
@@ -52,7 +53,16 @@ router.post("/login", async (req, res) => {
     // Hash our password and compare with the db one
     const result = await comparePassword(password, passFromDb);
     if (result) {
-      res.json({ status: "success", message: "Login Successfully" });
+      const accessJWT = await createAccessJWT(user.email);
+
+      const refreshJWT = await createRefreshJWT(user.email);
+
+      res.json({
+        status: "success",
+        message: "Login Successfully",
+        accessJWT,
+        refreshJWT,
+      });
     } else {
       return res.json({ status: "error", message: "Invalid password!" });
     }
